9627a466ebbb02f562ba27b871f0dd3a7d2298b2
kiwoong96 <justlikethat8778@gmail.com>
2021-03-07 23:28:30 +0900

    2021.03.07

--- 그래프 탐색/2206번_벽 부수고 이동하기★★★★★/solution.py
+++ 그래프 탐색/2206번_벽 부수고 이동하기★★★★★/solution.py
@@ -27,30 +27,39 @@ dy = [0,0,1,-1]
 s_x,s_y = 0,0
 
 def solution(board,s_x,s_y):
-    visited = [[[False,False] for _ in range(M)]for _ in range(M)]
+    visited = [[[False,False] for _ in range(M)]for _ in range(N)]
     result = -1
     q = deque()
     chance = False
     cnt = 0
     q.append((cnt,chance,s_x,s_y))
-    visited[s_x][s_y] = True
-    print_board(board)
+    visited[s_x][s_y][0] = True ##방문기억, 벽을 부수고 진행했나?
     while q:
         print(q)
+
         cnt, chance, x, y = q.popleft()
         if x == N-1 and y == M-1:
-            result = cnt
+            result = cnt + 1
         for i in range(4):
             tmp_x, tmp_y = x+dx[i], y+dy[i]
-            print(tmp_x,tmp_y)
-            if 0<=tmp_x<N and 0<=tmp_y<M and visited[tmp_x][tmp_y] == False:
-                print("Come in",tmp_x,tmp_y)
-                if board[tmp_x][tmp_y] == 0:
-                    q.append((cnt+1,chance, tmp_x,tmp_y))
+            if 0<=tmp_x<N and 0<=tmp_y<M and visited[tmp_x][tmp_y][0] == False:
 
+                if board[tmp_x][tmp_y] == 0:
+                    visited[tmp_x][tmp_y][0] = True
+                    q.append((cnt+1,chance, tmp_x,tmp_y))
+                    if chance == False:
+                        visited[tmp_x][tmp_y] == False
                 elif board[tmp_x][tmp_y] == -1 and chance == False:
+                    visited[tmp_x][tmp_y][0] = True
+                    visited[tmp_x][tmp_y][1] = True
                     tmp_chance = True
                     q.append((cnt+1,tmp_chance,tmp_x,tmp_y))
+            elif 0<=tmp_x<N and 0<=tmp_y<M and visited[tmp_x][tmp_y][0] == True:
+                print(tmp_x,tmp_y, visited[tmp_x][tmp_y], chance)
+                if board[tmp_x][tmp_y] == 0 and visited[tmp_x][tmp_y][1] == True and chance == False:
+                    print(tmp_x,tmp_y)
+                    q.append((cnt+1,chance,tmp_x,tmp_y))
+
 
 
 
