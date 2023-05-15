83085774a2325b6c4516165f585cdf2cd301aad7
kiwoong96 <justlikethat8778@gmail.com>
2023-05-16 00:48:54 +0900

    2023.05.16

--- Lv3/디스크 컨트롤러/solution.py
+++ Lv3/디스크 컨트롤러/solution.py
@@ -2,13 +2,24 @@ import heapq
 #[[0, 3], [1, 9], [2, 6]]	9
 def solution(jobs):
     answer = 0
-    hq = []
-    for job in jobs:
-        heapq.heappush(hq,(job[1], job[0]))
+    now = 0 #현재시간
+    i = 0   #처리개수
+    start = -1 #마지막 완료시간
+    heap = []
     
-    now_time = 0
-    while q:
+    while i < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(heap,[job[1],job[0]])
         
-    
-    
-    return answer
+        if heap:
+            time, start_time = heapq.heappop(heap)
+            start = now
+            now += time
+            answer += now - start_time #요청으로부터 처리시간
+            i += 1
+        else:
+            now += 1
+            
+    return answer // len(jobs)
+
